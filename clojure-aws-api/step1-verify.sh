a6bccd196d77d8cdafd6f3a41a3a1614758db1dd
felixfon <felix.fonteneau@hotmail.com>
2021-04-25 19:05:06 +0200

    fix

--- clojure-aws-api/step1-verify.sh
+++ clojure-aws-api/step1-verify.sh
@@ -1,2 +1,3 @@
-[ -d /home/scrapbook/tutorial/aws-api-tutorial && -f /home/scrapbook/tutorial/aws-api-tutorial/project.clj && -f /home/scrapbook/tutorial/aws-api-tutorial/src/aws_api_tutorial/core.clj] && echo "done"
+[ -d /home/scrapbook/tutorial/aws-api-tutorial ] && [-f /home/scrapbook/tutorial/aws-api-tutorial/project.clj] && [-f /home/scrapbook/tutorial/aws-api-tutorial/src/aws_api_tutorial/core.clj] && echo "done"
 
+<
