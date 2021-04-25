45a5796010f70fa33eb943f7492c2b9d6711fbf0
felixfon <felix.fonteneau@hotmail.com>
2021-04-25 19:28:38 +0200

    fix

--- clojure-aws-api/step2-verify.sh
+++ clojure-aws-api/step2-verify.sh
@@ -1,6 +1,8 @@
-if [ grep -q '[com.cognitect.aws/api "0.8.505"]' /root/aws-api-tutorial/project.clj ]
-    && [ grep -q '[com.cognitect.aws/endpoints "1.1.11.976"]' /root/aws-api-tutorial/project.clj ]
-    && [ grep -q '[com.cognitect.aws/lambda "811.2.865.0"]' /root/aws-api-tutorial/project.clj ]
-    && [ grep -q '[com.cognitect.aws/iam "811.2.889.0"]' /root/aws-api-tutorial/project.clj ]; then
+if  grep -q '[com.cognitect.aws/api "0.8.505"]' /root/aws-api-tutorial/project.clj ; then
   echo "done"
 fi
+
+
+#    && [ grep -q '[com.cognitect.aws/endpoints "1.1.11.976"]' /root/aws-api-tutorial/project.clj ]
+#    && [ grep -q '[com.cognitect.aws/lambda "811.2.865.0"]' /root/aws-api-tutorial/project.clj ]
+#    && [ grep -q '[com.cognitect.aws/iam "811.2.889.0"]' /root/aws-api-tutorial/project.clj ]
