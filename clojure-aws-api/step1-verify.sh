4ddcbdf48316e8965dd4f84c36f014fbc0e0fbe3
felixfon <felix.fonteneau@hotmail.com>
2021-04-26 09:48:54 +0200

    fix the verifications

--- clojure-aws-api/step1-verify.sh
+++ clojure-aws-api/step1-verify.sh
@@ -1,3 +1,3 @@
-if [ -d /root/aws-api-tutorial ] && [ -f /root/aws-api-tutorial/project.clj ] && [ -f /root/aws-api-tutorial/src/aws_api_tutorial/core.clj ] && [[ $(pwd) == /root/aws-api-tutorial ]] ; then
+if [ -d /root/aws-api-tutorial ] && [ -f /root/aws-api-tutorial/project.clj ] && [ -f /root/aws-api-tutorial/src/aws_api_tutorial/core.clj ] ; then
   echo "done"
 fi
